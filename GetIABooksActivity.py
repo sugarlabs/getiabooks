3e78b8a35dcf25b628f7a8df1e46ce897ed7658f
Aniket21mathur <amathur@ec.iitr.ac.in>
2019-01-03 08:51:07 +0530

    Removing shortcut line from code
    Since the default shortcut is defined in the toolkit, there is no
    need to set it in the activity code.It gets imported with the stop button.

--- GetIABooksActivity.py
+++ GetIABooksActivity.py
@@ -290,7 +290,6 @@ class GetIABooksActivity(activity.Activity):
         separator.show()
 
         stop_button = StopButton(self)
-        stop_button.props.accelerator = '<Ctrl>Q'
         toolbar_box.toolbar.insert(stop_button, -1)
         stop_button.show()
 
